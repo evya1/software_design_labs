b05faffeec46ab2ceb59c03b66459f99c2305701
evya1 <103887518+evya1@users.noreply.github.com>
2024-05-01 12:33:28 +0300

    Completed Task 3: Refactor ArithmeticApp.java for Improved Readability and Functionality
    - Removed inline comments and replaced them with documentation comments for methods.
    - Removed unused import statements and interface definitions.
    - Restructured code to enhance readability and maintainability.
    - Added documentation comments summarizing the purpose and behavior of methods.
    - Improved error handling and added appropriate error messages for invalid expressions and unsupported bases.
    - Closed the scanner object to prevent resource leaks.
    - Modified getBaseInput method to handle input without waiting for a new line character.
    - Updated solve method to return the result as a string representation in the specified base.
    - Modified session method to properly handle exceptions for division by zero and empty expressions.

--- src/main/java/org/example/lab1/ArithmeticApp.java
+++ src/main/java/org/example/lab1/ArithmeticApp.java
@@ -1,53 +1,6 @@
 package org.example.lab1;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
-
-interface Operation {
-    int doOperation(int a, int b);
-}
-
-// class representing custom exception
-class ValidityException extends Exception {
-    public ValidityException(String msg) {
-        // calling the constructor of parent Exception
-        super(msg);
-
-    }
-}
-
-class Add implements Operation {
-    @Override
-    public int doOperation(int a, int b) {
-        return a + b;
-    }
-}
-
-class Subtract implements Operation {
-    @Override
-    public int doOperation(int a, int b) {
-        return a - b;
-    }
-}
-
-class Multiply implements Operation {
-    @Override
-    public int doOperation(int a, int b) {
-        return a * b;
-    }
-}
-
-class Divide implements Operation {
-    @Override
-    public int doOperation(int a, int b) {
-        if (b == 0) {
-            throw new ArithmeticException("Cannot divide by zero");
-        }
-        return a / b;
-    }
-}
-
+import java.util.*;
 
 public class ArithmeticApp {
 
@@ -55,45 +8,173 @@ public class ArithmeticApp {
     public static final int OCTAL_BASE = 8;
     public static final int DECIMAL_BASE = 10;
     public static final int HEX_BASE = 16;
+    public static final char ADDITION = '+';
+    public static final char SUBTRACTION = '-';
+    public static final char MULTIPLICATION = '*';
+    public static final char DIVISION = '/';
 
     public static final int[] supportedBases = {BINARY_BASE, OCTAL_BASE, DECIMAL_BASE, HEX_BASE};
+    public static final List<Character> supportedOperators = new ArrayList<>(List.of(ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION));
+    public static Scanner scanner = new Scanner(System.in);
 
+    /**
+     * Validates if the given base is supported.
+     *
+     * @param base The base to validate.
+     * @return {@code true} if the base is supported, {@code false} otherwise.
+     * @throws ValidityException If the base is not supported.
+     */
+    private static boolean isValidBase(int base) throws ValidityException {
+        for (int supportedBase : supportedBases) {
+            if (base == supportedBase) {
+                return true;
+            }
+        }
+        throw new ValidityException("Error – this base isn’t supported.");
+    }
+
+
+    /**
+     * Checks if the given string representation is valid for the specified base.
+     *
+     * @param str  The string representation to check.
+     * @param base The base in which to interpret the string representation.
+     * @return {@code true} if the string representation is valid for the given base, {@code false} otherwise.
+     */
+    public static boolean isInGivenBase(String str, int base) {
+        try {
+            Integer.parseInt(str, base);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Validates an arithmetic expression and extracts its parts (operands and operators).
+     * <p>
+     * This method checks whether the given arithmetic expression is valid for evaluation in the specified base.
+     * It removes any whitespace characters from the expression and checks for operators at the beginning
+     * and end of the expression. Then, it splits the expression into parts (operands and operators)
+     * and adds them to the provided list. If any part of the expression contains an invalid character,
+     * it throws an {@code ArithmeticException}.
+     * </p>
+     *
+     * @param expression The arithmetic expression to validate and extract parts from.
+     * @param base       The base of the numeric operands in the expression.
+     * @param parts      The list to store the extracted expression parts (operands and operators).
+     * @throws ArithmeticException If the expression contains an invalid character or operator at the beginning or end.
+     */
+    private static void isValidExpression(String expression, int base, ArrayList<String> parts) throws ArithmeticException {
+        expression = expression.replaceAll(" ", "");
+        final char firstCharInExpression = expression.charAt(0);
+        final char lastCharInExpression = expression.charAt(expression.length() - 1);
+
+        if (supportedOperators.contains(firstCharInExpression) || supportedOperators.contains(lastCharInExpression)) {
+            throw new ArithmeticException();
+        }
+
+        String[] expressionParts = expression.split("(?=[-+*/])|(?<=[-+*/])");
+
+        for (String part : expressionParts) {
+            if (isInGivenBase(part, base)) {
+                parts.add(part);
+            } else if (supportedOperators.contains(part.charAt(0))) {
+                parts.add(part);
+            } else {
+                throw new ArithmeticException();
+            }
+        }
+    }
+
+    /**
+     * Evaluates an arithmetic expression represented by a list of expression parts and returns the result.
+     * <p>
+     * This method evaluates the given arithmetic expression in the specified base. It uses two stacks:
+     * one for operands and another for operators. It iterates through each part of the expression,
+     * pushing operands onto the operands stack and performing operations when encountering operators.
+     * After processing all parts of the expression, it applies any remaining operators to the operands
+     * and returns the final result as a string representation in the specified base.
+     * </p>
+     *
+     * @param expressionParts The list of expression parts (operands and operators).
+     * @param base            The base of the numeric operands in the expression.
+     * @return The result of evaluating the expression as a string representation in the specified base.
+     */
+    private static String solve(ArrayList<String> expressionParts, int base) {
+        Stack<Integer> operandsStack = new Stack<>();
+        Stack<Character> operatorsStack = new Stack<>();
+
+        for (String part : expressionParts) {
+            if (supportedOperators.contains(part.charAt(0))) {
+                char operator = part.charAt(0);
+                while (!operatorsStack.isEmpty() && hasPriorityOver(operator, operatorsStack.peek())) {
+                    operandsStack.push(applyOperator(operatorsStack.pop(), operandsStack.pop(), operandsStack.pop()));
+                }
+                operatorsStack.push(operator);
+            } else {
+                operandsStack.push(Integer.parseInt(part, base));
+            }
+        }
+
+        while (!operatorsStack.isEmpty()) {
+            operandsStack.push(applyOperator(operatorsStack.pop(), operandsStack.pop(), operandsStack.pop()));
+        }
+
+        int result = operandsStack.pop();
+
+        return Integer.toString(result, base).toUpperCase();
+    }
+
+
+    /**
+     * Determines whether the second operator has higher precedence than or equal precedence to the first operator.
+     *
+     * @param firstOperand  The first operator.
+     * @param secondOperand The second operator.
+     * @return {@code true} if the second operator has higher or equal precedence compared to the first operator; {@code false} otherwise.
+     */
+    private static boolean hasPriorityOver(char firstOperand, char secondOperand) {
+        return (firstOperand != MULTIPLICATION && firstOperand != DIVISION) || (secondOperand != ADDITION && secondOperand != SUBTRACTION);
+    }
+
+    /**
+     * Applies the specified operator on the given operands and returns the result.
+     *
+     * @param operator The operator to apply. It can be one of the following: '+', '-', '*', or '/'.
+     * @param operandB The second operand.
+     * @param operandA The first operand.
+     * @return The result of applying the operator on the operands.
+     * @throws UnsupportedOperationException If attempting to divide by zero when the operator is '/'.
+     */
+    public static int applyOperator(char operator, int operandB, int operandA) {
+        return switch (operator) {
+            case ADDITION -> operandA + operandB;
+            case SUBTRACTION -> operandA - operandB;
+            case MULTIPLICATION -> operandA * operandB;
+            case DIVISION -> {
+                if (operandB == 0) throw new UnsupportedOperationException();
+                yield operandA / operandB;
+            }
+            default -> 0;
+        };
+    }
 
     private static int getBaseInput(String s) {
-        Scanner scanner = new Scanner(System.in);
         System.out.println(s);
         return scanner.nextInt();
     }
 
     private static String getExpressionInput() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter expression:");
         return scanner.nextLine();
     }
 
-    private static boolean isValidBase(int base) throws ValidityException {
-        boolean validBase = Arrays.asList(supportedBases).contains(base);   // BUG: doesn't assign correct boolean to validBase
-        if (!validBase) {
-            String msg = "Error – this base isn’t supported.";
-            throw new ValidityException(msg);
-        }
-        return true;
-    }
-
-    private static void isValidExpression(String expression) {
-//       Whatever Logic should be here...
-    }
-
-    private static void solve(String expression) {
-
-        String msg = "The value of expression";
-        System.out.println(msg + expression);
-
-    }
-
     public static void session() {
         int base = getBaseInput("Enter base (2/8/10/16):");
-        boolean valid = false;
+        scanner.nextLine();
+        boolean valid;
+        String solution;
         try {
             valid = isValidBase(base);
         } catch (ValidityException e) {
@@ -106,14 +187,35 @@ public class ArithmeticApp {
             }
         }
         if (valid) {
+            // Create a mutable object to hold the expression, allowing for modification if necessary
             String expression = getExpressionInput();
-            isValidExpression(expression);
-            solve(expression); // factory....
+            ArrayList<String> expressionParts = new ArrayList<>();
+            try {
+                isValidExpression(expression, base, expressionParts);
+                solution = solve(expressionParts, base);
+                String msg = "The value of expression " + expression + " is : " + solution;
+                System.out.println(msg);
+            } catch (ArithmeticException e) {
+                System.out.println("invalid expression: " + expression);
+            } catch (UnsupportedOperationException e) {
+                System.out.println("Error: trying to divide by 0 (evaluated: \"0\")");
+            } catch (EmptyStackException e) {
+                System.out.println("Error: invalid expression: \"\"");
+            }
         }
-
+        scanner.close();
     }
 
-    public static void main (String[]args){
+
+    public static void main(String[] args) {
         ArithmeticApp.session();
     }
+
+    static class ValidityException extends Exception {
+        public ValidityException(String msg) {
+            super(msg);
+        }
+    }
+
 }
+
